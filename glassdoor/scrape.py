c73ad04a6b5bba05e69dbc81f4ef889ab236a7e9
Sean Softcheck <ssoftcheck@gmail.com>
2019-04-16 00:53:03 -0400

    potential new tag/class names

--- glassdoor/scrape.py
+++ glassdoor/scrape.py
@@ -29,7 +29,8 @@ result_df = None
 while not expired:
     reviews = content.find_all('div',{'class':'hreview'})
 
-    datetimes = [dateparse.parse(x.find('time', {'class':'date subtle small'}).attrs['datetime']) for x in reviews]
+    datetimes = [x.find('time', {'class':'date subtle small'}) for x in reviews]
+    datetimes = [dateparse.parse(x.attrs['datetime']) if x is not None else None for x in datetimes]
     
     helpful_count = [x.find('span', {'class':'helpfulCount subtle'}) for x in reviews]
     helpful_count = [re.search(r' *helpful *\((?P<count>\d+)\)',x.text,re.IGNORECASE).group('count') if x is not None else None for x in helpful_count]
@@ -38,7 +39,7 @@ while not expired:
 
     stars = list(map(lambda x: x.find('span', {'class':'value-title'}).attrs['title'], reviews))
 
-    subratings = [x.find('div', {'class':'subRatings module'}) for x in reviews]
+    subratings = [x.find('div', {'class':'subRatings module stars__StarsStyles__subRatings'}) for x in reviews]
     
     subratings_title = [x.find_all('span',{'class':'gdBars gdRatings med'}) if x is not None else None for x in subratings]
     subratings_title = [[y.attrs['title'] for y in x] if x is not None else None for x in subratings_title]
@@ -63,7 +64,7 @@ while not expired:
     if 'Work/Life Balance' in subratings:
         subratings['work_life_rating'] = subratings.pop('Work/Life Balance')
     if 'Career Opportunities' in subratings:
-            subratings['career_rating'] = subratings.pop('Career Opportunities')
+        subratings['career_rating'] = subratings.pop('Career Opportunities')
     if 'Culture & Values' in subratings:
         subratings['culture_rating'] = subratings.pop('Culture & Values')
     if 'Senior Management' in subratings:
@@ -73,7 +74,7 @@ while not expired:
     author_status = list(map(lambda x: x[0], author))
     author_role = list(map(lambda x: x[1], author))
 
-    recommend = [x.find('div',{'class':'flex-grid recommends'}) for x in  reviews]
+    recommend = [x.find('div',{'class':'row reviewBodyCell recommends'}) for x in reviews]
     
     outlook_positive = [x.find('span',string='Positive Outlook') if x is not None else None for x in recommend]
     outlook_neutral = [x.find('span',string='Neutral Outlook') if x is not None else None for x in recommend]
@@ -91,25 +92,31 @@ while not expired:
     recommends_negative = [x.text if x is not None else '' for x in recommends_negative]
     recommended = [x + y for x,y in zip(recommends_positive, recommends_negative)]
     
-    ceo_approve = [x.find('span',{'class':'showDesk'}, string='Approves of') if x is not None else None for x in recommend]
-    ceo_neutral = [x.find('span',{'class':'showDesk'}, string='No opinion of') if x is not None else None for x in recommend]
-    ceo_disapprove = [x.find('span',{'class':'showDesk'}, string='Disapproves of') if x is not None else None for x in recommend]
+    ceo_approve = [x.find('span', string='Approves of CEO') if x is not None else None for x in recommend]
+    ceo_neutral = [x.find('span', string='No opinion of CEO') if x is not None else None for x in recommend]
+    ceo_disapprove = [x.find('span', string='Disapproves of CEO') if x is not None else None for x in recommend]
     
     ceo_approve = [x.text if x is not None else '' for x in ceo_approve]
     ceo_neutral = [x.text if x is not None else '' for x in ceo_neutral]
     ceo_disapprove = [x.text if x is not None else '' for x in ceo_disapprove]
     approve_ceo = [(x + y + z).replace(' of','') for x,y,z in zip(ceo_approve, ceo_neutral, ceo_disapprove)]
 
-    longevity = list(map(lambda x: x.find('p',{'class':'tightBot mainText'}), reviews))
+    longevity = list(map(lambda x: x.find('p',{'class':'mainText mb-0'}), reviews))
     longevity = [re.findall(r'at [\w\d]+ (.+)',x.text.replace('\xa0',u' '), re.IGNORECASE+re.DOTALL)[0] if x is not None else '' for x in longevity]
 
-    text_main = list(map(lambda x: x.find('div', {'class':'tbl fill prosConsAdvice truncateData'}), reviews))
-    text_pros = list(map(lambda x: x.find('p', {'class':'pros mainText truncateThis wrapToggleStr'}), text_main))
-    text_pros = [' '.join([str(y) for y in x.contents]) if x is not None else '' for x in text_pros]
-    text_cons = list(map(lambda x: x.find('p', {'class':'cons mainText truncateThis wrapToggleStr'}), text_main))
-    text_cons = [' '.join([str(y) for y in x.contents]) if x is not None else '' for x in text_cons]
-    text_advice = list(map(lambda x: x.find('p', {'class':'adviceMgmt mainText truncateThis wrapToggleStr'}), text_main))
-    text_advice = [' '.join([str(y) for y in x.contents]) if x is not None else '' for x in text_advice]
+    text_main = list(map(lambda x: x.find('div', {'class':'description'}).find_all('div',{'class':'mt-md'}), reviews))
+        
+    text_pros = map(lambda x: [y.find('p',{'class':'strong'},string='Pros') for y in x], text_main)
+    text_pros = map(lambda x: [y.next_sibling.text if y is not None else '' for y in x], text_pros)
+    text_pros = list(map(lambda x: reduce(lambda a,b: a+b,x), text_pros))
+    
+    text_cons = map(lambda x: [y.find('p',{'class':'strong'},string='Cons') for y in x], text_main)
+    text_cons = map(lambda x: [y.next_sibling.text if y is not None else '' for y in x], text_cons)
+    text_cons = list(map(lambda x: reduce(lambda a,b: a+b,x), text_cons))
+    
+    text_advice = map(lambda x: [y.find('p',{'class':'strong'},string='Advice to Management') for y in x], text_main)
+    text_advice = map(lambda x: [y.next_sibling.text if y is not None else '' for y in x], text_advice)
+    text_advice = list(map(lambda x: reduce(lambda a,b: a+b,x), text_advice))
     
     result = pd.DataFrame({'date':datetimes, 'helpful':helpful_count, 'headline':headlines, 'overall_rating':stars, **subratings,
           'author_status':author_status, 'author_role':author_role,
@@ -120,9 +127,9 @@ while not expired:
     else:
         result_df = pd.concat([result_df,result])
 
-    next_page = content.find('li', {'class':'next'}).find('a')
+    next_page = content.find('link', {'rel':'next'})
     if next_page is not None:
-        get_url = parent_url.format(next_page.attrs['href'])
+        get_url = next_page.attrs['href']
         req = request.Request(get_url, None, headers)
         gd_resp = request.urlopen(req)
         content = bsoup(gd_resp.read(), 'html.parser')
